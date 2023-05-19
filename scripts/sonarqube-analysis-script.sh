952e3706c9d4d3d099c16c617919d5a8a27e0e34
Git-Gouse123 <shaik.gsb3@gmail.com>
2023-05-19 09:33:40 +0000

    sonarqube access token provided

--- scripts/sonarqube-analysis-script.sh
+++ scripts/sonarqube-analysis-script.sh
@@ -2,6 +2,6 @@
 
 # Run SonarQube analysis with GATE conditions
 sonar-scanner \
-  -Dsonar.host.url=<SonarQube URL> \
-  -Dsonar.login=<SonarQube token> \
+  -Dsonar.host.url=<http://35.154.61.125:9000/> \
+  -Dsonar.login=<squ_ec001b48f1bd3012ff21f56a6cd83749e5f3ff26> \
   -Dsonar.qualitygate.wait=true
