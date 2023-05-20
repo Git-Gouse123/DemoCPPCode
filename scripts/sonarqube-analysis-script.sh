70f13ecf7db21bf168e135a03380c447b89117a4
Git-Gouse123 <133126585+Git-Gouse123@users.noreply.github.com>
2023-05-20 16:45:34 +0530

    Update sonarqube-analysis-script.sh

--- scripts/sonarqube-analysis-script.sh
+++ scripts/sonarqube-analysis-script.sh
@@ -2,6 +2,8 @@
 
 # Run SonarQube analysis with GATE conditions
 sonar-scanner \
-  -Dsonar.host.url=<http://35.154.61.125:9000/> \
-  -Dsonar.login=<squ_ec001b48f1bd3012ff21f56a6cd83749e5f3ff26> \
+  -Dsonar.host.url=http://35.154.61.125:9000/ \
+  -Dsonar.login=squ_ec001b48f1bd3012ff21f56a6cd83749e5f3ff26 \
+  -Dsonar.projectKey=demo \
+  -Dsonar.source=src \
   -Dsonar.qualitygate.wait=true
